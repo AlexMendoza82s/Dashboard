32a7bc5bb27b9bbdf0b1d7118ddc7f43bd85904f
alexmendoza82s <alexmendoza_22s@hotmail.com>
2023-02-07 20:32:23 -0500

    Actualizacion

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,84 +1,3 @@
-var ctx = document.getElementById("chart-bars").getContext("2d");
-
-new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["M", "T", "W", "T", "F", "S", "S"],
-    datasets: [{
-      label: "Sales",
-      tension: 0.4,
-      borderWidth: 0,
-      borderRadius: 4,
-      borderSkipped: false,
-      backgroundColor: "rgba(255, 255, 255, .8)",
-      data: [50, 20, 10, 22, 50, 10, 40],
-      maxBarThickness: 6
-    }, ],
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: false,
-      }
-    },
-    interaction: {
-      intersect: false,
-      mode: 'index',
-    },
-    scales: {
-      y: {
-        grid: {
-          drawBorder: false,
-          display: true,
-          drawOnChartArea: true,
-          drawTicks: false,
-          borderDash: [5, 5],
-          color: 'rgba(255, 255, 255, .2)'
-        },
-        ticks: {
-          suggestedMin: 0,
-          suggestedMax: 500,
-          beginAtZero: true,
-          padding: 10,
-          font: {
-            size: 14,
-            weight: 300,
-            family: "Roboto",
-            style: 'normal',
-            lineHeight: 2
-          },
-          color: "#fff"
-        },
-      },
-      x: {
-        grid: {
-          drawBorder: false,
-          display: true,
-          drawOnChartArea: true,
-          drawTicks: false,
-          borderDash: [5, 5],
-          color: 'rgba(255, 255, 255, .2)'
-        },
-        ticks: {
-          display: true,
-          color: '#f8f9fa',
-          padding: 10,
-          font: {
-            size: 14,
-            weight: 300,
-            family: "Roboto",
-            style: 'normal',
-            lineHeight: 2
-          },
-        }
-      },
-    },
-  },
-});
-
-
 var ctx2 = document.getElementById("chart-line").getContext("2d");
 
 new Chart(ctx2, {
@@ -254,9 +173,87 @@ if (win && document.querySelector('#sidenav-scrollbar')) {
 
 let loadData =()=>
 {
-    let newData = [100, 80, 60, 40, 20, 10, 1];
+    var newData = [100, 80, 60, 40, 20, 10, 1];
     let chartRef1=document.getElementById("chart-bars").getContext("2d");
-    let chart1=new Chart(chartRef1,{});
+    let chart1 = new Chart(chartRef1, 
+    {
+      type: "bar",
+      data: {
+        labels: ["M", "T", "W", "T", "F", "S", "S"],
+        datasets: [{
+          label: "Sales",
+          tension: 0.4,
+          borderWidth: 0,
+          borderRadius: 4,
+          borderSkipped: false,
+          backgroundColor: "rgba(255, 255, 255, .8)",
+          data: [50, 20, 10, 22, 50, 10, 40],
+          maxBarThickness: 6
+        }, ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false,
+          }
+        },
+        interaction: {
+          intersect: false,
+          mode: 'index',
+        },
+        scales: {
+          y: {
+            grid: {
+              drawBorder: false,
+              display: true,
+              drawOnChartArea: true,
+              drawTicks: false,
+              borderDash: [5, 5],
+              color: 'rgba(255, 255, 255, .2)'
+            },
+            ticks: {
+              suggestedMin: 0,
+              suggestedMax: 500,
+              beginAtZero: true,
+              padding: 10,
+              font: {
+                size: 14,
+                weight: 300,
+                family: "Roboto",
+                style: 'normal',
+                lineHeight: 2
+              },
+              color: "#fff"
+            },
+          },
+          x: {
+            grid: {
+              drawBorder: false,
+              display: true,
+              drawOnChartArea: true,
+              drawTicks: false,
+              borderDash: [5, 5],
+              color: 'rgba(255, 255, 255, .2)'
+            },
+            ticks: {
+              display: true,
+              color: '#f8f9fa',
+              padding: 10,
+              font: {
+                size: 14,
+                weight: 300,
+                family: "Roboto",
+                style: 'normal',
+                lineHeight: 2
+              },
+            }
+          },
+        },
+      },
+    });
+    chart1.data.datasets[0].data=newData;
 
     let chartRef2=document.getElementById("chart-line").getContext("2d");
     let chart2=new Chart(chartRef2,{});
